418443d0e37fd7ffb9e5b7f18a60882a7df49920
archer <econarcher2010@gmail.com>
2024-03-24 06:03:31 +0800

    add background

--- web2/src/app/page.jsx
+++ web2/src/app/page.jsx
@@ -1,13 +1,20 @@
 import Card from "./component/Card";
 import Header from "./component/Header";
+import axios from "axios";
+
 
 export default function page(second) {
+  
+
+
+
+
   return (
     <>
       <div className="mx-auto max-w-screen-2xl">
         <Header homepage={true} />
-        
-        <h2 className="mx-auto max-w-screen-2xl text-2xl font-bold">
+        <img className="block mx-auto" src="./banner.jpg" alt="banner" />
+        <h2 className="mx-auto mb-2 mt-6 max-w-screen-2xl text-2xl font-bold">
           人氣民宿、公寓類型住宿
         </h2>
         <ul className="mx-auto flex max-w-screen-2xl flex-wrap ">
@@ -54,6 +61,9 @@ export default function page(second) {
             description={"hello world"}
           />
         </ul>
+        <div style={{background:"#E1E6E0"}}>
+          <p style={{ height: "400px", textAlign:"center",paddingTop:"60px", fontSize:"30px" }}>今天開始訂閱加入，開啟一趟旅行！</p>
+        </div>
       </div>
     </>
   );
