8b0c80894e58e8f63d6d6e020f7d8a1ea2d5e4fc
archer <econarcher2010@gmail.com>
2024-03-23 20:41:56 +0800

    create Home page

--- web2/src/app/component/Card.jsx
+++ web2/src/app/component/Card.jsx
@@ -1,11 +1,13 @@
 export default function Card({ room_name, price, image, rating, description }) {
   return (
-    <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="mx-2 my-4 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img class="p-2 rounded-t-lg" src={image} alt="product image" />
       </a>
       <div class="px-5 pb-5">
-        <p class="text-xl font-bold">{room_name}</p>
+        <a href="">
+          <p class="text-xl font-bold">{room_name}</p>
+        </a>
         <a href="#">
           <h5 class="text-xl tracking-tight text-gray-900 dark:text-white">
             {description}
--- web2/src/app/home/page.jsx
+++ web2/src/app/home/page.jsx
@@ -2,20 +2,52 @@ import Card from "../component/Card";
 
 export default function page(second) {
   return (
-    <div className="className="mx-auto max-w-screen-2xl bg-myFifthColor>
-      <Card
-        room_name={"SINGLE"}
-        price={599}
-        image={"./cat.jpg"}
-        rating={4.8}
-        description={"hello world"}
-      />
-      <Card
-        price={599}
-        image={"./cat.jpg"}
-        rating={4.8}
-        description={"hello world"}
-      />
+    <div className="className=" mx-auto max-w-screen-2xl>
+      <ul className="mx-auto flex max-w-screen-2xl flex-wrap ">
+        
+        <Card
+          room_name={"SINGLE"}
+          price={599}
+          image={"./cat.jpg"}
+          rating={4.8}
+          description={"hello world"}
+        />
+        <Card
+          room_name={"SINGLE"}
+          price={599}
+          image={"./cat.jpg"}
+          rating={4.8}
+          description={"hello world"}
+        />
+        <Card
+          room_name={"SINGLE"}
+          price={599}
+          image={"./cat.jpg"}
+          rating={4.8}
+          description={"hello world"}
+        />
+        <Card
+          room_name={"SINGLE"}
+          price={599}
+          image={"./cat.jpg"}
+          rating={4.8}
+          description={"hello world"}
+        />
+        <Card
+          room_name={"SINGLE"}
+          price={599}
+          image={"./cat.jpg"}
+          rating={4.8}
+          description={"hello world"}
+        />
+        <Card
+          room_name={"SINGLE"}
+          price={599}
+          image={"./cat.jpg"}
+          rating={4.8}
+          description={"hello world"}
+        />
+      </ul>
     </div>
   );
 }
