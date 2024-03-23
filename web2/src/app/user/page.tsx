c49b26878a0cc20c6246c5289ca5b02fb58f0017
etblh <hugo432121@gmail.com>
2024-03-23 18:15:44 +0800

    solved package.json merge conflict

--- web2/src/app/user/page.tsx
+++ web2/src/app/user/page.tsx
@@ -1,7 +1,23 @@
+import { MenuItem, MenuList } from "@mui/material";
+
 const UserPage = () => {
     return (
         <div id="user-page">
-                        
+            <header>
+                <span>LOGO</span>
+            </header>
+            <main style={{display:"flex"}}>
+                <nav style={{width: '20vw'}}>
+                    <MenuList>
+                        <MenuItem>
+                            User Info
+                        </MenuItem>
+                    </MenuList>
+                </nav>
+                <div style={{flex: 1}}>
+
+                </div>
+            </main>
         </div>
     )
 }
